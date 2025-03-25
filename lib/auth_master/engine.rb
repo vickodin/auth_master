9709849d23e60fe7a79e47a75c2e18385c2c4da6
vickodin <vick.orel@gmail.com>
2025-03-25 20:42:39 +0300

    Layout: drop it from the engine

--- lib/auth_master/engine.rb
+++ lib/auth_master/engine.rb
@@ -1,5 +1,13 @@
 module AuthMaster
   class Engine < ::Rails::Engine
+    DEFAULT_LAYOUT = "application"
+
     isolate_namespace AuthMaster
+
+    config.to_prepare do
+      ApplicationController.layout ->(controller) {
+        AuthMaster.targets[params[:target].to_sym][:layout] || DEFAULT_LAYOUT
+      }
+    end
   end
 end
