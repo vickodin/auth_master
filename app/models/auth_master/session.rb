3f00f6adb38afa06daf92fa18be86f008c1463de
vickodin <vick.orel@gmail.com>
2025-03-27 12:41:29 +0300

    Sessions: Add enum status (0.0.3)

--- app/models/auth_master/session.rb
+++ app/models/auth_master/session.rb
@@ -1,5 +1,7 @@
 module AuthMaster
   class Session < ApplicationRecord
     belongs_to :target, polymorphic: true
+
+    enum :status, [ :inactive, :active ], default: :inactive
   end
 end
--- lib/auth_master/version.rb
+++ lib/auth_master/version.rb
@@ -1,3 +1,3 @@
 module AuthMaster
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
