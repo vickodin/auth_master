9bced72cd4a26f48e3622df2545b008f793517a8
vickodin <vick.orel@gmail.com>
2025-03-28 12:02:33 +0300

    Activation: Cleaning

--- app/services/auth_master/session_service.rb
+++ app/services/auth_master/session_service.rb
@@ -5,7 +5,6 @@ module AuthMaster
 
     DEFAULT_LOGIN_TIMEOUT_INTERVAL  = 5.minutes
     DEFAULT_LOGIN_ATTEMPTS_COUNT    = 3
-    DEFAULT_TOKEN_PURPOSE           = :auth_master_email
 
     class << self
       def create!(target, uuid:)
@@ -21,10 +20,6 @@ module AuthMaster
       def activate!(auth_master_session)
         auth_master_session.active!
         # TODO: Save IP Address, User Agent, etc
-      end
-
-      def check_token(token)
-        # auth_master_session_id = TokenGuard.decrypt(token, purpose: token_purpose(target), secret:)
       end
 
       private
@@ -45,21 +40,8 @@ module AuthMaster
         AuthMaster.targets[target_name(target)][:login_attempts_count] || DEFAULT_LOGIN_ATTEMPTS_COUNT
       end
 
-      def token_purpose(target)
-        # binding.irb
-        AuthMaster.targets[target_name(target)][:token_purpose] || DEFAULT_TOKEN_PURPOSE
-      end
-
       def target_name(target)
         target.class.to_s.downcase.to_sym
-      end
-
-      def target_mailer(target)
-        config_for(target, :mailer_class).to_s.classify.constantize
-      end
-
-      def target_mailer_login_link_method(target)
-        config_for(target, :mailer_login_link_method)
       end
 
       def config_for(target, name)
