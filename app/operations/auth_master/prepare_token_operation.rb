4d12537393134e04716bbf6998c9341859fbc456
vickodin <vick.orel@gmail.com>
2025-03-31 19:46:06 +0300

    Try use auth_master as gem (not engine)

---
+++ app/operations/auth_master/prepare_token_operation.rb
@@ -0,0 +1,26 @@
+module AuthMaster
+  class PrepareTokenOperation < AuthMaster::AbstractOperation
+    def self.call!(target, uuid:)
+      auth_master_session = AuthMaster::SessionService.create!(target, uuid:)
+      return if auth_master_session.blank?
+
+      purpose = token_purpose_config(target)
+      secret  = secret_config(target)
+
+      TokenGuard.encrypt(auth_master_session.id, purpose:, secret:)
+
+      # mailer = target_mailer_config(target)
+      # mailer_action = target_mailer_login_link_method(target)
+
+      # url = AuthMaster::Engine.routes.url_helpers.auth_master_link_url(
+      #   target: target_name(target),
+      #   token: token,
+      #   host: Rails.application.config.action_mailer.default_url_options[:host]
+      # )
+
+      # mailer.with(email: target.email, url:).public_send(mailer_action).deliver_later
+
+      # auth_master_session
+    end
+  end
+end
