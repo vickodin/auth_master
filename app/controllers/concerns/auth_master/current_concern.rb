ac2db7a2b7b3d70a9bf920f55a76254f7cbf9afb
Victor <vick.orel@gmail.com>
2025-04-30 08:06:15 +0300

    Usual Gem (#7)

--- app/controllers/concerns/auth_master/current_concern.rb
+++ app/controllers/concerns/auth_master/current_concern.rb
@@ -1,11 +1,7 @@
 module AuthMaster::CurrentConcern
   extend ActiveSupport::Concern
 
-  # included do
-  #   helper_method :current_auth_master
-  # end
-
-  def current_auth_master(target_param_name)
+  def auth_master_current(target_param_name)
     session_accessor_key = [ "current", target_param_name, "id" ].join("_")
     auth_master_session_id = session[session_accessor_key]
     return nil if auth_master_session_id.blank?
@@ -18,5 +14,46 @@ module AuthMaster::CurrentConcern
     # return nil if !target.is_a?(target_class_by_name(target_param_name))
 
     target
+  end
+
+  def auth_master_prepare_token(target)
+    return if target.blank?
+    return if !target.persisted?
+
+    uuid = auth_master_session_id_generator
+    session[auth_master_session_key(target.class)] = uuid
+
+    AuthMaster::PrepareTokenOperation.call!(target, uuid:)
+  end
+
+  def auth_master_login_by_token(target_class, token)
+    uuid = session[auth_master_session_key(target_class)]
+
+    auth_master_session = AuthMaster::LoginByTokenOperation.call!(token, uuid:, target_class:)
+    return if auth_master_session.blank?
+
+    session.delete(auth_master_session_key(target_class))
+    session[auth_master_target_session_key(target_class)] = auth_master_session.id
+
+    true
+  end
+
+  def auth_master_logout(target_class)
+    auth_master_session_id = session.delete(auth_master_target_session_key(target_class))
+    AuthMaster::LogoutOperation.call!(auth_master_session_id)
+  end
+
+  private
+
+  def auth_master_session_key(target_class)
+    [ "auth_master", target_class.name.underscore, "id" ].join("_")
+  end
+
+  def auth_master_target_session_key(target_class)
+    [ "current", target_class.name.underscore, "id" ].join("_")
+  end
+
+  def auth_master_session_id_generator
+    Random.uuid
   end
 end
