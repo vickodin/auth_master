685f1af890e02184b4759e3e61cc37871eb662b0
vickodin <vick.orel@gmail.com>
2025-05-18 13:44:34 +0300

    14: Fixes

--- app/controllers/concerns/auth_master/current_concern.rb
+++ app/controllers/concerns/auth_master/current_concern.rb
@@ -22,7 +22,7 @@ module AuthMaster::CurrentConcern
 
     uuid = auth_master_session_id_generator
     target_class = target.respond_to?(:auth_master_config_class) ? target.auth_master_config_class : target.class
-    session[auth_master_session_key(target.class)] = uuid
+    session[auth_master_session_key(target_class)] = uuid
 
     AuthMaster::PrepareTokenOperation.call!(target, uuid:, purpose:)
   end
